c1a27a90fa21dcc1b740edad6119121c7f532fef
unknown <1947283246@qq.com>
2018-02-07 22:17:05 +0800

    第一次提交

---
+++ AutoReadMore.js
@@ -0,0 +1,7 @@
+﻿
+var btn = document.getElementsByClassName("btn btn-large btn-gray-fred read_more_btn");
+
+if (btn && btn.length == 1)
+{
+    btn[0].click();
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# CSDN_AutoReadMore
+
+火狐浏览器扩展，在 CSDN 博客页面自动 “阅读全文”。
+
+效果请看 before/after 对比图。
